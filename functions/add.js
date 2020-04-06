b0c2492cf9b73de4d25fa1e442504981278c7565
popsps <59066663+popsps@users.noreply.github.com>
2020-04-06 15:39:48 -0400

    functions update 2

--- functions/add.js
+++ functions/add.js
@@ -1,8 +1,11 @@
 exports.handler = function (event, context, callback) {
-  const {a, b} = context.clientContext
   console.log(context)
+  const {a, b} = context.clientContext
+  const c = parseInt(a) + parseInt(b)
   callback(null, {
     statusCode: 200,
-    body: a + b
+    body: JSON.stringify({
+      c: c
+    })
   });
 }
